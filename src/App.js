5e03b3b343535c8f12c4f721451a54a26075c5d5
stirbex <stirbexsrl@yahoo.com>
2019-02-21 23:14:16 +0200

    first save

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar.js';
+import Cart from './components/Cart.js';
+import Default from './components/Default.js';
+import Details from './components/Details.js';
+import ProductList from './components/ProductList.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
     );
   }
 }
